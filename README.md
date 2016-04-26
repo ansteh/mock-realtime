d7d5a758e299d4e84f8b0a1b329e62d467e9edca
ansteh <stehle.andre@googlemail.com>
2016-04-27 00:28:52 +0200

    integer generator biased by weekday as clause

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ mock real time data as JSON
 # preview
 ```js
 'use strict';
-const Day = require('mock-realtime').Day;
+const realtime = require('mock-realtime');
+const Day = realtime.Day;
 
 let now = Day({
   hours: { min: 10, max: 13},
@@ -16,6 +17,19 @@ let now = Day({
 console.log(now.get().toString());
 console.log(now.next().toString());
 console.log(now.next().toString());
+```
+
+# range integer biased by weekday
+```js
+let biasedIntegerGenerator = exports.rangeIntegerBiasedByWeekdays(0, 20, {
+  0: { min: 0, max: 0 }, //sunday
+  6: { min: 0, max: 0 }  //saturday
+});
+
+for(var i=0; i<7; i+=1){
+  let current = now.next();
+  console.log(current.get().toString(), biasedIntegerGenerator(current.get()));
+}
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 'use strict';
 const moment = require('moment');
-const Random = require('random-js');
 const _      = require('lodash');
 
 const util   = require('./lib/util.js');
-const seeds = require('./lib/seeds.js');
-
-const range = function(min, max){
-  let engine = Random.engines.mt19937().autoSeed();
-  let distribution = Random.integer(min, max);
-  return function() {
-    return distribution(engine);
-  };
-};
+const seeds  = require('./lib/seeds.js');
+const number = require('./lib/number.js');
 
 const genIncrementer = _.curry(function(key, schema, date){
   let current = moment(date);
@@ -20,7 +12,7 @@ const genIncrementer = _.curry(function(key, schema, date){
   schema = util.filterSchemaBy(key, schema);
 
   _.forOwn(schema, function(options, name){
-    distributions[name] = range(options.min, options.max);
+    distributions[name] = util.range(options.min, options.max);
   });
 
   function get(){
@@ -55,6 +47,8 @@ _.forOwn(seeds, function(options, seedName){
     exports[name] = genIncrementer(seedName);
 });
 
+exports.rangeIntegerBiasedByWeekdays = number.rangeIntegerBiasedByWeekdays;
+
 //console.log(seeds.days.max().toString());
 //console.log(_.chain('days').capitalize().trimEnd('s').value());
 //const Day = genIncrementer('days');
@@ -62,13 +56,24 @@ _.forOwn(seeds, function(options, seedName){
 
 /*let Day = exports.Day;
 let now = Day({
-  hours: { min: 10, max: 13},
-  minutes: { min: 0, max: 59},
-  seconds: { min: 0, max: 59},
-  milliseconds: { min: 0, max: 999}
+  hours: { min: 10, max: 13 },
+  minutes: { min: 0, max: 59 },
+  seconds: { min: 0, max: 59 },
+  milliseconds: { min: 0, max: 999 }
 }, Date.now());
 
 console.log(now.get().toString());
 console.log(now.next().toString());
-console.log(now.next().toString());*/
+console.log(now.next().toString());
 //console.log(exports);
+
+
+let biasedIntegerGenerator = exports.rangeIntegerBiasedByWeekdays(0, 20, {
+  0: { min: 0, max: 0 },
+  6: { min: 0, max: 0 }
+});
+
+for(var i=0; i<7; i+=1){
+  let current = now.next();
+  console.log(current.get().toString(), biasedIntegerGenerator(current.get()));
+}*/
